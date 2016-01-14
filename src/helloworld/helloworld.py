a79a6818259854995799c7e742ae88cee3044792
Peter C Pearson <ppearson@unix5.andrew.cmu.edu>
2016-01-14 15:54:53 -0500

    ANOTHER MESSAGE

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -10,16 +10,10 @@ def main():
     print """
     Hello World, from...
     Joel D. Welling
-<<<<<<< HEAD
     Adam Williams
-=======
-<<<<<<< HEAD
     Art Wetzel
-=======
     William R. Howard Matchen
     Peter C. Pearson
->>>>>>> 92ffd723fa98f8579c9a682f8f0aaf3b4f694437
->>>>>>> 1723e7588b9ed15fc2bbd6271b1974db019dc5ad
     """
 
 
